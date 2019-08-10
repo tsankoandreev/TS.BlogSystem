9c7d7eba119863d2c73a4aa061237f5effafbe34
tsankoandreev <tsankoandreev@hotmail.com>
2019-08-10 17:01:37 +0300

    -removed Services project //services will stay at Core // -add 2 service interfaces for categories and posts

---
+++ src/TS.BlogSystem.Core/Interfaces/Services/IPostService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TS.BlogSystem.Core.Entities;
+
+namespace TS.BlogSystem.Core.Interfaces.Services
+{
+    public interface IPostService
+    {
+        /// <summary>
+        /// Delete post
+        /// </summary>
+        /// <param name="post">Post</param>
+        void DeletePost(Post post);
+        /// <summary>
+        /// Gets a post
+        /// </summary>
+        /// <param name="postId">Post identifier</param>
+        /// <returns>Post</returns>
+        Post GetPostById(Guid postId);
+
+        /// <summary>
+        /// Inserts post
+        /// </summary>
+        /// <param name="post">Post</param>
+        void InsertPost(Post post);
+
+        /// <summary>
+        /// Updates the post
+        /// </summary>
+        /// <param name="post">Post</param>
+        void UpdatePost(Post post);
+
+        /// <summary>
+        /// Retrieve all posts
+        /// </summary>
+        /// <returns></returns>
+        List<Post> GetAll();
+
+    }
+}
