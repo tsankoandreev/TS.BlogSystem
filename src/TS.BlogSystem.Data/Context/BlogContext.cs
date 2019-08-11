19b3dbff7a02b0ccffd79001e5dfa64c63fd2a78
tsankoandreev <tsankoandreev@hotmail.com>
2019-08-11 17:07:54 +0300

    - add db context

---
+++ src/TS.BlogSystem.Data/Context/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TS.BlogSystem.Core.Entities;
+using TS.BlogSystem.Core.Interfaces.Context;
+
+namespace TS.BlogSystem.Data.Context
+{
+    public class BlogContext : DbContext, IBlogContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
