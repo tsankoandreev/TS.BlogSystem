623e90f38ad50110d765395652dbc9168fb5a6ef
tsankoandreev <tsankoandreev@hotmail.com>
2019-08-13 19:33:56 +0300

    - updated EFCore package in Data proj. - add UserRoles for user

--- src/TS.BlogSystem.Core/Entities/User.cs
+++ src/TS.BlogSystem.Core/Entities/User.cs
@@ -7,7 +7,7 @@ namespace TS.BlogSystem.Core.Entities
 {
     public class User : IdentityUser<Guid>
     {
-
+        public IList<UserRole> Roles { get; set; } = new List<UserRole>();
         public virtual ICollection<Post> Posts { get; set; }
     }
 }
