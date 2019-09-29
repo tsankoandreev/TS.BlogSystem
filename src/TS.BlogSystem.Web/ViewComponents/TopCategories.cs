cb10b59df7ff7ff5edbbad8f3f4ef3e31ed26fa1
tsankoandreev <tsankoandreev@hotmail.com>
2019-09-29 09:16:38 +0300

    - refactored Layout - 2 column (8x4) - add TopCategories View component

---
+++ src/TS.BlogSystem.Web/ViewComponents/TopCategories.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TS.BlogSystem.Core.Interfaces.Services;
+using TS.BlogSystem.Web.Models;
+
+namespace TS.BlogSystem.Web.ViewComponents
+{
+    public class TopCategories : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public TopCategories(ICategoryService categoryService)
+        {
+            this._categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int numberToTake)
+        {
+            var categories = await _categoryService.GetAll();
+            var result = categories.Take(numberToTake).Select(x => new CategoryViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+            }).ToList();
+
+            return await Task.FromResult((IViewComponentResult)View(result));
+        }
+    }
+}
