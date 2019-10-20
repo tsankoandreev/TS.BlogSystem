c1f4c4e0fad449cf3de3fae86b9ba2a7738cea8e
tsankoandreev <tsankoandreev@hotmail.com>
2019-10-20 11:57:43 +0300

    add Admin Area with AdminLTE template

--- src/TS.BlogSystem.Web/Startup.cs
+++ src/TS.BlogSystem.Web/Startup.cs
@@ -69,8 +69,13 @@ namespace TS.BlogSystem.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areaRoute",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
             });
         }
