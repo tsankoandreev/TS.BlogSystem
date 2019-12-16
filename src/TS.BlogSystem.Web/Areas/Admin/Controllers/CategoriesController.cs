a3c1d8b5356eafcfdfa7b64b6591c14742bd64d0
tsankoandreev <tsankoandreev@hotmail.com>
2019-12-16 17:54:02 +0200

    - add categories controller in admin panel with CRUD for Category

---
+++ src/TS.BlogSystem.Web/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TS.BlogSystem.Core.Entities;
+using TS.BlogSystem.Core.Interfaces.Services;
+using TS.BlogSystem.Data.Context;
+
+namespace TS.BlogSystem.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        // GET: Admin/Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _categoryService.GetAll());
+        }
+
+        // GET: Admin/Categories/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _categoryService.GetById(id.Value);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description,IsLocked,DateCreated,Id")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Id = Guid.NewGuid();
+                await _categoryService.Insert(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _categoryService.GetById(id.Value);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Name,Description,IsLocked,DateCreated,Id")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(category);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _categoryService.GetById(id.Value);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var category = await _categoryService.GetById(id);
+            await _categoryService.Delete(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return _categoryService.GetById(id) != null;
+        }
+    }
+}
